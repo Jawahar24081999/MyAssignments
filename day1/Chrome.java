23eb3917a3e6f22646064f6fb7cf7c5c18015af0
Jawahar24081999 <jawahar.baskar@gmail.com>
2025-07-19 21:57:59 +0530

    Add files via upload
    day 1

---
+++ day1/Chrome.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		int year=2008;
+		System.out.println(year);
+	
+	float rupee=91.012345678f;
+	System.out.println(rupee);
+	String name="Google";
+	System.out.println(name);
+	boolean beta=false;
+	System.out.println(beta);
+	char value='C';
+	System.out.println(value);
+	
+	 
+	}
+
+	
+
+}
