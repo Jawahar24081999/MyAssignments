67e06616bc6bc2b97a92ab15b3bf047489aed9b7
Jawahar24081999 <jawahar.baskar@gmail.com>
2025-08-20 01:10:02 +0530

    Add files via upload

---
+++ day2/FrameAndAlertInteraction.java
@@ -0,0 +1,31 @@
+package weak4.day2;
+
+
+	import java.time.Duration;
+
+	import org.openqa.selenium.Alert;
+	import org.openqa.selenium.By;
+	import org.openqa.selenium.chrome.ChromeDriver;
+
+	public class FrameAndAlertInteraction{
+
+		public static void main(String[] args) {
+			ChromeDriver driver = new ChromeDriver();
+			driver.get("https://www.w3schools.com/js/tryit.asp?filename=tryjs_prompt");
+			driver.manage().window().maximize();
+			driver.switchTo().frame(0);
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		    driver.findElement(By.tagName("button")).click();
+			Alert promptAlert = driver.switchTo().alert();
+			promptAlert.sendKeys("Jawahar");
+			promptAlert.accept();
+		    String action = driver.findElement(By.id("demo")).getText();
+			if (action.contains("cancelled")) {
+				System.out.println("Cancel button clicked");
+			} else
+				System.out.println("Ok button clicked");
+		}
+
+	}
+
+
